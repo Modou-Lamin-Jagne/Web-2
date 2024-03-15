6c5ceb49a2ede45e8bc433ecf94db35974f3751c
ModouLaminJagne <jagnemlg@gmai.com>
2024-03-15 13:07:23 +0000

    Refactor math operations and add conditional statements for grading

--- main.js
+++ main.js
@@ -2,29 +2,29 @@
 
 // Path: index.html
 // const message = prompt("Enter a message: ");
-// const myHeading = document.querySelector('h1');
+const myHeading = document.querySelector('h1');
 // myHeading.textContent = message;
 
 // creating variables to perform mathematics operations
 
-let x = 10;
-let y = 5;
+// let x = 10;
+// let y = 5;
 
-let addition = x + y;
-console.log(addition);
-alert(addition)
+// let addition = x + y;
+// console.log(addition);
+// alert(addition)
 
-let subtraction = x - y;
-console.log(subtraction);
-alert(subtraction)
+// let subtraction = x - y;
+// console.log(subtraction);
+// alert(subtraction)
 
-let multiplication = x * y;
-console.log(multiplication);
-alert(multiplication)
+// let multiplication = x * y;
+// console.log(multiplication);
+// alert(multiplication)
 
-let division = x / y;
-console.log(division);
-alert(division)
+// let division = x / y;
+// console.log(division);
+// alert(division)
 
 
 // Conditional Statements
@@ -40,22 +40,22 @@ alert(division)
  * 0 - 39: F 
  */
 
-// let grade = Number(prompt("Enter your grade: "));
+let grade = Number(prompt("Enter your grade: "));
 
-// if (grade >= 90 && grade <= 100) {  // A+
-//     myHeading.textContent = "A+";
-// } else if (grade >= 80 && grade <= 89) {  // A
-//     myHeading.textContent = "A";
-// } else if (grade >= 70 && grade <= 79) {  // A-
-//     myHeading.textContent = "A-";
-// } else if (grade >= 60 && grade <= 69) {  // B
-//     myHeading.textContent = "B";
-// } else if (grade >= 50 && grade <= 59) {  // C
-//     myHeading.textContent = "C";
-// } else if (grade >= 40 && grade <= 49) {  // D
-//     myHeading.textContent = "D";
-// } else if (grade >= 0 && grade <= 39) {  // F
-//     myHeading.textContent = "F";
-// } else {
-//     myHeading.textContent = "Invalid Grade";
-// }
+if (grade >= 90 && grade <= 100) {  // A+
+    myHeading.textContent = "A+";
+} else if (grade >= 80 && grade <= 89) {  // A
+    myHeading.textContent = "A";
+} else if (grade >= 70 && grade <= 79) {  // A-
+    myHeading.textContent = "A-";
+} else if (grade >= 60 && grade <= 69) {  // B
+    myHeading.textContent = "B";
+} else if (grade >= 50 && grade <= 59) {  // C
+    myHeading.textContent = "C";
+} else if (grade >= 40 && grade <= 49) {  // D
+    myHeading.textContent = "D";
+} else if (grade >= 0 && grade <= 39) {  // F
+    myHeading.textContent = "F";
+} else {
+    myHeading.textContent = "Invalid Grade";
+}
