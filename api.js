e70ba923364e9accc8776c83997d009cbef44cbe
ModouLaminJagne <jagnemlg@gmai.com>
2024-05-03 11:05:05 +0000

    feat: Add functionality to load courses table
    This commit adds the necessary JavaScript code to load the courses table in the courses.html page. It includes the function loadCoursesTable() which retrieves the course details from the input fields and inserts a new row into the table with the corresponding data. The table columns include course name, course code, credit hours, department, and lecturer.

--- api.js
+++ api.js
@@ -59,6 +59,39 @@ function loadLecturersTable(){
     cell7.innerHTML = contact;
 }
 
+function loadCoursesTable(){
+
+    var Ctable = document.getElementById("CdataTable");
+
+    var course_name = document.getElementById("course_name").value;
+    var course_code = document.getElementById("course_code").value;
+    var credit_hours = document.getElementById("credite_hours").value;
+    var department = document.getElementById("department").value;
+    var lecturer = document.getElementById("lecturer").value;
+    // var address = document.getElementById("l_address").value;
+    // var contact = document.getElementById("contact").value;
+
+
+    var newRow = Ctable.insertRow(-1);
+
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1)
+    var cell3 = newRow.insertCell(2)
+    var cell4 = newRow.insertCell(3)
+    var cell5 = newRow.insertCell(4)
+    // var cell6 = newRow.insertCell(5)
+    // var cell7 = newRow.insertCell(6)
+
+    cell1.innerHTML = course_name;
+    cell2.innerHTML = course_code;
+    cell3.innerHTML = credit_hours;
+    cell4.innerHTML = department;
+    cell5.innerHTML = lecturer;
+    // cell6.innerHTML = address;
+    // cell7.innerHTML = contact;
+}
+
 function clear(){
     var first_name = document.getElementById("first_name");
     var middle_name = document.getElementById("middle_name");
@@ -68,6 +101,12 @@ function clear(){
     var gender = document.getElementById("gender");
     var address = document.getElementById("address");
     var contact = document.getElementById("address");
+    var course_name = document.getElementById("course_name");
+    var lecturer = document.getElementById("lecturer");
+    var department = document.getElementById("department");
+    var credit_hours = document.getElementById("credite_hours");
+    var course_code = document.getElementById("course_code");
+    
     first_name = '';
     middle_name = '';
     last_name = '';
@@ -76,4 +115,9 @@ function clear(){
     gender = '';
     address = '';
     contact = '';
+    course_name = '';
+    course_code = '';
+    credit_hours = '';
+    department = '';
+    lecturer = '';
 }
